6cd73c29b7f0e541e07e4af7eff36037ecc00244
EmekaBest <josephemeka2611@gmail.com>
2024-04-12 18:36:52 +0100

    Finished the logic on verification page

--- src/Pages/Verification/VerifyEmail.jsx
+++ src/Pages/Verification/VerifyEmail.jsx
@@ -6,9 +6,16 @@ import React from 'react';
 // This class is used to make sure the dom(defined in the "render() function") is loaded first before implementing the code in "componentDidMount()"
   class VerfyEmail extends React.Component {
     componentDidMount() {
-
+     /**Dom elements definition center...........................................................................*/
       const inputs = document.querySelectorAll(".inputs");
-      
+      const verifyBtn = document.querySelector('.containerInnerRightInnerChild5')
+      ///////////////////////////////////////////////////////////////////////////
+
+      /**Global varables declaration center .............*/
+      let allInputValues = "";
+      ////////////////////////////////////////////////
+
+
       if (inputs.length){
         //initial index position of inputs to focus
         let i = 0;
@@ -22,24 +29,74 @@ import React from 'react';
           inputs.forEach((input)=>{
           
             input.addEventListener("keyup", (e)=>{
-              i++;//incrementing i each time a value is inserted in an input
 
-              if (i < inputs.length){
-                inputs[i].focus();//focuses on an input based on the value of i at the moment
+              const regexNum = /[0-9]$/ 
+              const aNumber = regexNum.test(input.value)
+            
+              /**This condition deprecates any non numerical value from being inserted and increment the value of i if false*/
+              if (!aNumber ){
+                
+                input.value = ""
+              }
+              else{
+                  i++;//incrementing i each time a value is inserted in an input
+                }
+                ///////////////////////////////////////////////////////////////////////
+
+                //when the result of this condition is true, all value in the inputs are cleared
+              if (e.key === "Backspace"){
+                i = 0;
+                inputs.forEach((input)=>{
+                  input.value = "";
+                  allInputValues = ""
+                  input.classList.remove('errorCode')
+
+                })
+              }
+              ///////////////////////////////////////////////////////////////////////////////
+
+               if (i < inputs.length){
+                
+               inputs[i].focus()//Focuses on the next input after an input is inserted
 
               }
-              
+
             })
 
           })
-      
       }
+
+      /**This action takes place when the verify button is clicked */
+      verifyBtn.addEventListener('click', ()=>{
+        inputs.forEach((input)=>{
+          allInputValues += input.value 
+
+        })
+       
+        const verficationCodeSent = "123456"//This is just a testing reference. A real verification code will be used on this variable soon...
+        if (!(allInputValues == verficationCodeSent)){
+          
+          inputs.forEach((input)=>{
+            input.classList.add('errorCode')
+          })
+          inputs[5].focus()//focus the cursor on the last button
+
+          //alert('invalid verfication code')
+        }
+        else{
+          //Navigate to the page 
+
+        }
+        
+      })
       
     }
 
+    
+
 
     render(){
-      
+
       return(
 
         <div className="container">
