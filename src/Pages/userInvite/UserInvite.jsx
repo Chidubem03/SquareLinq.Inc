4e8ddda5819306231911dd4051b342ba57bef94e
welan616 <chukwumelapraise@gmail.com>
2024-04-21 12:38:53 +0100

    copy and paste feature added to the invite link page

--- src/Pages/userInvite/UserInvite.jsx
+++ src/Pages/userInvite/UserInvite.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import shineLogo from '../Images/shineLogo.png';
 import slider from '../Images/auth-pagination-slider4.png';
 import link from '../Images/link-i.png';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
 import './userinvite.css'
 
 export default function UserInvite() {
@@ -28,6 +29,17 @@ export default function UserInvite() {
             setErrMsg(null)
         }
      }
+     const [inviteLink, setInviteLink] = useState("www.linqsocial.com\\dummyShared\\linkPost");
+     const [copied, setCopied] = useState(false);
+     const handleCopy = () => {
+      setCopied(true);
+      setTimeout(() => setCopied(false), 2000); // Reset copied state after 2 seconds
+    };
+   function handleChange(event) {
+    const value = event.target.value;
+      setInviteLink(value)
+    }
+
   return (
     <div className='user-invite-container'>
       <div className='user-invite-info'>
@@ -47,12 +59,17 @@ export default function UserInvite() {
             <div className='invite-container-input'>
             <input type="text" 
             placeholder='www.linqsocial.com\shaniahTwain\sharednewPost' 
-            className='invite-inputs'/>
-            <img src={link} alt="" />
+            className='invite-inputs'
+            value={inviteLink}
+            onChange={handleChange}
+            />
+            <CopyToClipboard text={inviteLink} onCopy={handleCopy}>
+            {copied ? <span className="tiny-txt">copied!</span> : <img src={link} alt="" />}
+            </CopyToClipboard>
             </div>
             <p id="or">OR</p> 
             <div className='invite-container-input'>
-            <input type="text"
+            <input type="email"
             onChange={emailHandler}
             placeholder='Enter email to send an invite to your friends' 
             className='invite-inputs'
