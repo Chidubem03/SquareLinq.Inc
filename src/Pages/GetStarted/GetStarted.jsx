1430450429a41ac4bdfb55f40f3d4564bc7344d3
Ifeoluwa <ifel1939@gmail.com>
2024-04-21 03:39:10 +0100

    creating getstarted component

--- src/Pages/GetStarted/GetStarted.jsx
+++ src/Pages/GetStarted/GetStarted.jsx
@@ -12,6 +12,37 @@ import { IoIosClose, IoIosCheckmark } from "react-icons/io";
 import { Link } from "react-router-dom/cjs/react-router-dom";
 
 const GetStarted = () => {
+  const suggestion = [
+    {
+      img: "https://img.freepik.com/free-photo/positive-carefree-woman-with-curly-hair-dressed-hoodie-smiles-happily-makes-peace-gesture-takes-selfie-urban-place-being-good-mood-after-sport-training-people-emotions-sporty-lifestyle_273609-59906.jpg?size=626&ext=jpg&ga=GA1.1.1633137608.1702649795&semt=sph",
+      name: "John Doe",
+    },
+    {
+      img: "https://shotkit.com/wp-content/uploads/bb-plugin/cache/selfie-poses-landscape-52ad92635f1133a439ce85fd13a5bbd8-zybravgx2q47.jpg",
+      name: "Esther  Ben",
+    },
+    {
+      img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTu9C9664zPg5QP8jtHqYXJiizBdxd8LMTJbBwoEw3kVZSkmKDnkbUhawO2pP1KFknh99Q&usqp=CAU",
+      name: " Emmanuel Johsua",
+    },
+    {
+      img: "https://shotkit.com/wp-content/uploads/bb-plugin/cache/selfie-poses-landscape-52ad92635f1133a439ce85fd13a5bbd8-zybravgx2q47.jpg",
+      name: "Esther  Ben",
+    },
+    {
+      img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTu9C9664zPg5QP8jtHqYXJiizBdxd8LMTJbBwoEw3kVZSkmKDnkbUhawO2pP1KFknh99Q&usqp=CAU",
+      name: " Emmanuel Johsua",
+    },
+    {
+      img: "https://shotkit.com/wp-content/uploads/bb-plugin/cache/selfie-poses-landscape-52ad92635f1133a439ce85fd13a5bbd8-zybravgx2q47.jpg",
+      name: "Esther  Ben",
+    },
+    {
+      img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTu9C9664zPg5QP8jtHqYXJiizBdxd8LMTJbBwoEw3kVZSkmKDnkbUhawO2pP1KFknh99Q&usqp=CAU",
+      name: " Emmanuel Johsua",
+    },
+  ];
+
   return (
     <div className="getstarted-container">
       <div className="con interactive-sec">
@@ -39,10 +70,31 @@ const GetStarted = () => {
           <div className="import-div">
             <p>Import my contacts</p>
             <div className="toggle">
-              <span className="close-x"><IoIosClose/></span>
-              <span className="ok-x"><IoIosCheckmark className="check-icon"/></span>
+              <span className="close-x">
+                <IoIosClose />
+              </span>
+              <span className="ok-x">
+                <IoIosCheckmark className="check-icon" />
+              </span>
             </div>
           </div>
+
+          <main className="suggested-div">
+            <h3>Suggested friends</h3>
+            <article>
+              <div>
+                <img
+                  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTu9C9664zPg5QP8jtHqYXJiizBdxd8LMTJbBwoEw3kVZSkmKDnkbUhawO2pP1KFknh99Q&usqp=CAU"
+                  alt=""
+                className="suggested-img" />
+                <p className="suggested-name"> Emmanuel Joshua</p>
+              </div>
+
+              <div>
+                <IoIosCheckmark className="check-icon" />
+              </div>
+            </article>
+          </main>
         </article>
       </section>
     </div>
